805949907b5c37a2a1b2e5145b4a862548c6a43c
Igor Polishchuk <amadare42@gmail.com>
2020-01-24 17:45:49 +0200

    Updates

---
+++ precache-manifest.7bc23048210b615febfa499ba0b42013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35357aa28c5e0018fe22429129464b20",
+    "url": "/pathologic-companion/index.html"
+  },
+  {
+    "revision": "f18e0c45c1b9e5496d9c",
+    "url": "/pathologic-companion/static/css/2.8ff545c9.chunk.css"
+  },
+  {
+    "revision": "912b9bde33250b06af5b",
+    "url": "/pathologic-companion/static/css/main.aaecfee7.chunk.css"
+  },
+  {
+    "revision": "f18e0c45c1b9e5496d9c",
+    "url": "/pathologic-companion/static/js/2.dc747878.chunk.js"
+  },
+  {
+    "revision": "018191446896e18a42ee12b27d8f6ab0",
+    "url": "/pathologic-companion/static/js/2.dc747878.chunk.js.LICENSE"
+  },
+  {
+    "revision": "912b9bde33250b06af5b",
+    "url": "/pathologic-companion/static/js/main.e48f1092.chunk.js"
+  },
+  {
+    "revision": "f4b89b697bc4a5874f22",
+    "url": "/pathologic-companion/static/js/runtime-main.0b14d1ea.js"
+  },
+  {
+    "revision": "1c03d420fdc592e6e5cc18deaa5e647f",
+    "url": "/pathologic-companion/static/media/hand.1c03d420.svg"
+  }
+]);
